1e2aba0122fd08e58d4489f659ff9854bcbcfa53
ranp20 <polancoranp2019@gmail.com>
2024-07-06 07:33:12 -0500

    Latest updates on the back and the front side

--- core/app/Http/Controllers/User/OrderController.php
+++ core/app/Http/Controllers/User/OrderController.php
@@ -177,7 +177,7 @@ class OrderController extends Controller{
 
       config(['app.timezone' => 'America/Lima']);
       date_default_timezone_set('America/Lima');
-      $newDateOrder = date("Y/m/d h:i:s A", strtotime($order['created_at']));
+      $newDateOrder = date("Y/m/d - h:i:s A", strtotime($order['created_at']));
 
       $get_SessionUserInfo = [
         'date' => $newDateOrder,
